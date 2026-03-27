2a27d5f3cce2df3f1b86ded60fabc540cbc74855
Matias10k40 <maquinterosye@uide.edu.ec>
2026-03-27 15:23:47 +0000

    filesystem

---
+++ file-system_script.sh
@@ -0,0 +1,11 @@
+cd /dev
+#change directory to dev directory
+ls 
+#list archives and directory
+cd /proc
+#chance directory to proc directory 
+ls 
+#list proc directory 
+cat cpuinfo
+#shows cpu information
+
